7642300bc1d10616db637f094479912a6944157f
Miles Kenner <mdk0109@yahoo.com>
2019-03-12 13:21:11 -0500

    front end home page set

---
+++ server.js
@@ -0,0 +1,16 @@
+var path = require("path");
+
+var express = require("express");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
